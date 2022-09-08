9d24c1d4f39a33b3199e943799590b84f4289a2f
Juan Manuel Fernandez Daza <juanferdaza23@gmail.com>
2022-09-08 15:21:54 -0500

    wip: add stages and testing manual step

--- lib/aws-cdk-pipeline-stack.ts
+++ lib/aws-cdk-pipeline-stack.ts
@@ -3,22 +3,36 @@ import { Construct } from 'constructs';
 import { 
   CodePipeline, 
   CodePipelineSource, 
-  ShellStep, 
+  ShellStep,
   ManualApprovalStep 
 } from 'aws-cdk-lib/pipelines';
+import { MyPipelineAppStage } from './stage';
 
 export class AwsCdkPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, 'Pipeline', {
+    const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'Testpipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('Vlect/aws-cdk-pipeline', 'main'),
+        input: CodePipelineSource.connection('Vlect/aws-cdk-pipeline', 'main', {
+          connectionArn: "",
+          triggerOnPush: true,
+        }),
         commands: ['npm ci',
                    'npm run build',
                    'npx cdk synth']
       })
-    })
+    });
+
+    const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
+      env: { account: '064669052383', region: 'us-east-1' },
+    }));
+
+    testingStage.addPost(new ManualApprovalStep('Manual approval before production'));
+
+    const prodStage = pipeline.addStage(new MyPipelineAppStage(this, 'prod', {
+      env: { account: '064669052383', region: 'us-east-1' },
+    }));
   }
 }
