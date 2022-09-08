45fc1a77020631c87176e0750b50f9bdd9b9857c
Juan Manuel Fernandez Daza <juanferdaza23@gmail.com>
2022-09-08 16:07:15 -0500

    add another pipeline

--- lib/aws-cdk-pipeline-stack.ts
+++ lib/aws-cdk-pipeline-stack.ts
@@ -13,7 +13,7 @@ export class AwsCdkPipelineStack extends cdk.Stack {
     super(scope, id, props);
 
     const pipeline = new CodePipeline(this, 'Pipeline', {
-      pipelineName: 'Testpipeline',
+      pipelineName: 'Testpipeline1',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('Vlect/aws-cdk-pipeline', 'main'),
         commands: ['npm ci',
@@ -22,13 +22,37 @@ export class AwsCdkPipelineStack extends cdk.Stack {
       })
     });
 
+    
     const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
       env: { account: '064669052383', region: 'us-east-1' },
     }));
-
+    
     testingStage.addPost(new ManualApprovalStep('Manual approval before production'));
-
+    
     const prodStage = pipeline.addStage(new MyPipelineAppStage(this, 'prod', {
+      env: { account: '064669052383', region: 'us-east-1' },
+    }));
+
+    const pipeline1 = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'Testpipeline1',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.connection('Vlect/aws-cdk-pipeline', 'master', {
+          connectionArn: "arn:aws:codestar-connections:us-east-1:064669052383:connection/e7b07f1f-2237-4670-a8eb-aeeff638d610",
+          triggerOnPush: true,
+        }),
+        commands: ['npm ci',
+                   'npm run build',
+                   'npx cdk synth']
+      })
+    });
+
+    const testingStage1 = pipeline1.addStage(new MyPipelineAppStage(this, "test", {
+      env: { account: '064669052383', region: 'us-east-1' },
+    }));
+
+    testingStage1.addPost(new ManualApprovalStep('Manual approval before production'));
+
+    const prodStage1 = pipeline1.addStage(new MyPipelineAppStage(this, 'prod', {
       env: { account: '064669052383', region: 'us-east-1' },
     }));
   }
